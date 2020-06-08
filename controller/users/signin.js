5bcc632eb68717af6734f8344b0bd88866d3c7b9
oouuyab <01073785645@naver.com>
2020-06-09 00:15:50 +0900

    Add Token setting

--- controller/users/signin.js
+++ controller/users/signin.js
@@ -1,28 +1,28 @@
-const { users } = require('../../models')
+const { users } = require("../../models");
 
 module.exports = {
   post: async (req, res) => {
-    const { email, password } = req.body
-    var sess = req.session
+    const { email, password } = req.body;
+    var sess = req.session;
     await users
       .findOne({
         where: {
           email: email,
-          password: password
-        }
+          password: password,
+        },
       })
-      .then(result => {
+      .then((result) => {
         if (result === null) {
-          res.status(401).send('이메일이나 비밀번호가 일치하지 않습니다.')
+          res.status(401).send("이메일이나 비밀번호가 일치하지 않습니다.");
         } else {
-          sess.userid = result.id
+          sess.userid = result.id;
           res.status(200).json({
-            id: result.id
-          })
+            id: result.id,
+          });
         }
       })
-      .catch(err => {
-        res.status(404).send(err)
-      })
-  }
-}
+      .catch((err) => {
+        res.status(404).send(err);
+      });
+  },
+};
--- modules/checkToken.js
+++ modules/checkToken.js
@@ -2,6 +2,13 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
 
 export const checkToken = (req, res) => {
-  const token = req.cookies.token;
-  jwt.verify(token, process.env.JWTSECRETKEY, (error, decoded) => {});
+  const token_info = req.cookies.token;
+  jwt.verify(token, process.env.JWTSECRETKEY, (error, decoded) => {
+    if (err) {
+      return res.sendStatus(403);
+    } else {
+      return token;
+    }
+  });
+  return token_info;
 };
