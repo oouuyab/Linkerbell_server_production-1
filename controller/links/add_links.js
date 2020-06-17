3098b88de86416645f9e1aaba617f4b915850128
oouuyab <01073785645@naver.com>
2020-06-17 11:34:18 +0900

    :bug: Fixing bugs
    Fixing bugs

--- controller/links/add_links.js
+++ controller/links/add_links.js
@@ -4,50 +4,58 @@ const { checkToken } = require('../../modules');
 const extract = require('meta-extractor');
 module.exports = {
   post: async (req, res) => {
-    const token_info = checkToken(req);
-    const { user_id } = token_info;
-    const category = async function () {
-      return classifier(req.body.url);
-    };
-    const { result, analysis } = await category();
-    const ext = await extract({ uri: req.body.url });
-    urls
-      .findOrCreate({
-        where: {
-          user_id: user_id,
-          url: req.body.url,
-        },
-        defaults: {
-          category_id: result,
-          og_title: ext.ogTitle || ext.title || '',
-          og_image: ext.ogImage || ext.twitterImage || '',
-          og_description:
-            ext.ogDescription ||
-            ext.description ||
-            ext.twitterDescription ||
-            '',
-        },
-      })
-      .then(async ([url, created]) => {
-        if (!created) {
-          return res.status(409).send('이미 존재하는 url입니다.');
-        }
-        const data = await url.get({ plain: true });
-        res.status(201).send({ ...data, analysis: analysis });
-      });
+    try {
+      const token_info = checkToken(req);
+      const { user_id } = token_info;
+      const category = async function () {
+        return classifier(req.body.url);
+      };
+      const { result, analysis } = await category();
+      const ext = await extract({ uri: req.body.url });
+      urls
+        .findOrCreate({
+          where: {
+            user_id: user_id,
+            url: req.body.url,
+          },
+          defaults: {
+            category_id: result,
+            og_title: ext.ogTitle || ext.title || '',
+            og_image: ext.ogImage || ext.twitterImage || '',
+            og_description:
+              ext.ogDescription ||
+              ext.description ||
+              ext.twitterDescription ||
+              '',
+          },
+        })
+        .then(async ([url, created]) => {
+          if (!created) {
+            return res.status(409).send('이미 존재하는 url입니다.');
+          }
+          const data = await url.get({ plain: true });
+          res.status(201).send({ ...data, analysis: analysis });
+        });
+    } catch (err) {
+      return res.status(400).send('bad request');
+    }
   },
   delete: (req, res) => {
-    urls
-      .destroy({
-        where: {
-          id: req.params.url_id,
-        },
-      })
-      .then((result) => {
-        if (!result) {
-          res.status(400).send('bad request');
-        }
-        res.status(201).send('삭제되었습니다.');
-      });
+    try {
+      urls
+        .destroy({
+          where: {
+            id: req.params.url_id,
+          },
+        })
+        .then((result) => {
+          if (!result) {
+            res.status(400).send('bad request');
+          }
+          res.status(201).send('삭제되었습니다.');
+        });
+    } catch (err) {
+      res.status(400).send('bad request');
+    }
   },
 };
