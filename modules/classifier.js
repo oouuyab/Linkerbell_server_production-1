d63d82aec18eac45c73aa972a0bda7671447b741
oouuyab <01073785645@naver.com>
2020-06-21 14:08:54 +0900

    :bug: Fixing bugs.
    iframe 있을 시 puppeteer로 url 가져와서 cheerio적용 , watson api 적용 안됬을 때 카테고리 etc category_id 21로 리턴

--- modules/classifier.js
+++ modules/classifier.js
@@ -2,7 +2,6 @@ const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-unde
 const { IamAuthenticator } = require('ibm-watson/auth');
 const dotenv = require('dotenv').config();
 const category = require('./category');
-const crawler = require('./crawler');
 const cheerio = require('./cheerio');
 
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
@@ -15,26 +14,45 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
 
 const classifier = async (url) => {
   try {
-    console.time('classifier');
-    //const text = await crawler(url);
+    console.time('classifier time');
+    console.log('cheeio실행');
     const text = await cheerio(url);
-
-    console.log(text.length);
-    const analyzeParams = {
-      text: text,
-      //url: url,
-      features: {
-        categories: {
-          limit: 3,
+    console.log(text);
+    let analyzeParams;
+    if (text.length < 150) {
+      console.log(`text : ${text.length} < 150`);
+      analyzeParams = {
+        url: url,
+        features: {
+          categories: {
+            limit: 3,
+          },
         },
-      },
-    };
-
+      };
+    } else {
+      console.log(`text : ${text.length} > 150`);
+      analyzeParams = {
+        text: text,
+        features: {
+          categories: {
+            limit: 3,
+          },
+        },
+      };
+    }
+    console.log('newclassfier');
     const newClassifier = async () => {
       let goNLU = await naturalLanguageUnderstanding.analyze(analyzeParams);
       let analysisNLU = function () {
         let analysis = goNLU.result.categories;
-        let result = category(analysis)[0].label;
+        let result;
+        if (analysis.length === 0) {
+          result = 21;
+          analysis = [{ score: 0, label: '/etc' }];
+        } else {
+          result = category(analysis)[0].label;
+        }
+        console.log(result);
         return { result: result, analysis: analysis };
       };
       let resultNLU = await analysisNLU();
@@ -42,10 +60,11 @@ const classifier = async (url) => {
     };
     let result = await newClassifier();
     console.log(result);
-    console.timeEnd('classifier');
+    console.timeEnd('classifier time');
     return result;
   } catch (err) {
-    return { result: 0, analysis: [] };
+    console.log(err);
+    return { result: 21, analysis: [] };
   }
 };
 
