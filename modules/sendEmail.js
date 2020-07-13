4078fb4021d14bed39611d0eb68e5d7106e234f4
oouuyab <01073785645@naver.com>
2020-07-13 17:48:28 +0900

    :bug: fix sendEmail.js
    환경변수 설정

--- modules/sendEmail.js
+++ modules/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-let link = 'http://localhost';
+let link = process.env.SERVER_URL;
 let team_account = 'linkerbell.team7000@gmail.com';
 
 const transport = nodemailer.createTransport({
