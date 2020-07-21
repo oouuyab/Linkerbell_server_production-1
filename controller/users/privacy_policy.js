947d06dbfdb4ba639bb21c7bcbee0f59726d0793
oouuyab <01073785645@naver.com>
2020-07-21 23:18:49 +0900

    :pencil: Add privacy_policy.html
    privacy_policy 작성

--- controller/users/privacy_policy.js
+++ controller/users/privacy_policy.js
@@ -1,9 +1,21 @@
 const { users } = require('../../models');
+const fs = require('fs');
 
 module.exports = {
   get: (req, res) => {
     try {
-      res.status(200).send('provacy_policy');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      fs.readFile(
+        __dirname + '/../../asset/privacy_policy.html',
+        (err, data) => {
+          if (err) {
+            console.log('privacy_policy fs err');
+            console.log(err);
+            res.send('err');
+          }
+          res.status(200).end(data, 'utf-8');
+        }
+      );
     } catch (err) {
       console.log('privacy_policy err');
       console.log(err);
